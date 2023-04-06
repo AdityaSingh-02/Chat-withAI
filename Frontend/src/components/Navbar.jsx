dd451e9ef5386f3c59be3c8b5bb25625b8a5f468
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2023-04-06 10:10:10 +0530

    changes in front

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import "./NavbarStyles.css"
+const Navbar = () => {
+  return (
+    <center>
+    <div className="Main main">
+      <div className="Heading">
+        <h1 className="Nav-head">Navbar</h1>
+      </div>
+      <ul className="list-items">
+        <li className="ports port-home">
+            <Link to="/">Home</Link>
+        </li>
+        <li className="ports port-About">
+            <Link to="/about">About</Link>
+        </li>
+        <li className="ports port-Contact">
+            <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+    </center>
+  )
+}
+
+export default Navbar
