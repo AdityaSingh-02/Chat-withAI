e02bf896248a98fd2ed4b52111084d94e27add87
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2023-04-09 00:13:54 +0530

    changes

---
+++ Frontend/src/routes/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ErrorPage from "../components/ErrorPage"
+const Error = () => {
+  return (
+    <div>
+      <ErrorPage/>
+    </div>
+  )
+}
+
+export default Error
